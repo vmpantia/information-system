75ee54263987d60d6ff48b82316f7305305f5471
Vincent Pantia <vincent.m.pantia@hotmail.com>
2022-10-20 21:51:07 +0800

    [+] Add IS.Common

---
+++ IS.Common/IS.Logger/Logger.cs
@@ -0,0 +1,49 @@
+﻿using IS.Logger.Contractors;
+using IS.Logger.Models;
+
+namespace IS.Logger
+{
+    public class Logger : ILogger
+    {
+        private readonly string _filePath;
+        public Logger(string filePath)
+        {
+            _filePath = filePath;
+
+            if (!File.Exists(filePath))
+                File.Create(filePath).Dispose();
+        }
+
+        public async Task WriteInfoLog(string message)
+        {
+            string dateTime = DateTime.Now.ToString(Constants.LOG_DATE_TIME_FORMAT);
+
+            await File.AppendAllTextAsync(_filePath,
+                                          string.Concat(dateTime,
+                                                        "     INFO     ",
+                                                        message));
+        }
+
+        public async Task WriteWarnLog(string message)
+        {
+            string dateTime = DateTime.Now.ToString(Constants.LOG_DATE_TIME_FORMAT);
+
+            await File.AppendAllTextAsync(_filePath,
+                                          string.Concat(dateTime,
+                                                        "     WARN     ",
+                                                        message));
+        }
+
+        public async Task WriteErrorLog(string message, Exception exception)
+        {
+            string dateTime = DateTime.Now.ToString(Constants.LOG_DATE_TIME_FORMAT);
+
+            await File.AppendAllTextAsync(_filePath,
+                                          string.Concat(dateTime,
+                                                        "     ERROR     ",
+                                                        message,
+                                                        Globals.NEWLINE,
+                                                        exception.ToString()));
+        }
+    }
+}
---
+++ IS.Common/IS.Logger/Models/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS.Logger.Models
+{
+    internal class Constants
+    {
+        public const string LOG_DATE_TIME_FORMAT = "MM/dd/yyyy HH:mm:ss";
+    }
+}
